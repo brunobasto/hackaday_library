6e32f309ff14d61819167eeebc9cb81f4769a6d1
Inacio Nery <inacio.nery@liferay.com>
2016-08-19 15:18:51 -0300

    Init book Form

---
+++ web/src/main/dist/web/book/bookForm.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import templates from './bookForm.soy';
+
+class BookForm extends Component {
+
+
+}
+Soy.register(BookForm, templates);
+
+export default BookForm;
